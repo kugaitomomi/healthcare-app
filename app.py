950be1e52ea37bd7befcaffc1974aee5151c72e0
kugaitomomi <tottomo7@gmail.com>
2022-08-30 21:22:31 +0900

    feat: update and delete

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+from urllib import request
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
@@ -25,9 +26,58 @@ class Post(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
 
 
-@app.route('/create')
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template('create.html')
+    if request.method == 'POST':
+        upper_morning = request.form.get('upper_morning')
+        low_morning = request.form.get('low_morning')
+        upper_evening = request.form.get('upper_evening')
+        low_evening = request.form.get('low_evening')
+        pulse = request.form.get('pulse')
+        weight = request.form.get('weight')
+        medicine = request.form.get('medicine')
+        diary = request.form.get('diary')
+
+# フォームから取得した値は、dbのどのカラムに当てはまるのか指定してあげないといけない。
+        post = Post(upper_morning=upper_morning, low_morning=low_morning,
+                    upper_evening=upper_evening, low_evening=low_evening, pulse=pulse, weight=weight, medicine=medicine, diary=diary)
+
+        db.session.add(post)
+        db.session.commit()
+        return redirect('/')
+
+    else:
+        return render_template('create.html')
+
+
+@app.route('/<int:id>/update', methods=['GET', 'POST'])
+def update(id):
+    post = Post.query.get(id)
+    if request.method == 'GET':
+        return render_template('update.html', post=post)
+    else:
+        post.upper_morning = request.form.get('upper_morning')
+        post.low_morning = request.form.get('low_morning')
+        post.upper_evening = request.form.get('upper_evening')
+        post.low_evening = request.form.get('low_evening')
+        post.pulse = request.form.get('pulse')
+        post.weight = request.form.get('weight')
+        post.medicine = request.form.get('medicine')
+        post.diary = request.form.get('diary')
+
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/<int:id>/delete', methods=['GET'])
+def delete(id):
+    post = Post.query.get(id)
+
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
